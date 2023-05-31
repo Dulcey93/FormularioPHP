5f8bf591dea305f4c4ee22a30d52cbe5fcf12e4a
LuisSantiagoDulceyRamirezCampus <ldulcey93@unab.edu.co>
2023-05-31 08:53:50 -0500

    feat(Add code): :bug: Not working

--- model/api.php
+++ model/api.php
@@ -8,14 +8,13 @@ require("Clases/Calculadora.php");
 
 // Construir el algoritmo que solicite el nombre y edad de 3 personas y determine el nombre de la persona con mayor edad.
 header("Content-Type: application/json; charset:UTF-8");
-function ejecutar($num1, $num2) {
-    $calculadora = new Calculadora($num1, $num2);
-    $res= $num1>= $num2 ? $calculadora->sumarYRestar() : $calculadora->multiplicarYDividir();
+function ejecutar() {
+    $calculadora = new Calculadora();
     return $res;
   }
 
 $res = match ($METHOD) {
-    "POST" => ejecutar($num1, $num2)
+    "POST" => ejecutar()
 };
 
 $mensaje = [
